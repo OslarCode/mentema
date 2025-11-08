c33235725edf1ea2c2103c690177f6f059a4af1c
oscar <oslar1980@gmail.com>
2025-11-08 20:06:20 +0100

    fix: viteconfig

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,11 @@
 import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
+
 export default defineConfig(({ command }) => ({
   base: command === "build" ? "/mentema/" : "/",
   plugins: [tailwindcss()],
+  build: {
+    outDir: "docs",
+    emptyOutDir: true,
+  },
 }));
